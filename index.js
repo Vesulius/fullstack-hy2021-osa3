3e32b511bed16bfe55ea3dbb3614e9dfe7444693
Vesa <vesainwonderland@gmail.com>
2021-07-07 16:00:26 +0300

    3.6 puhelinluettelon backend

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ app.post('/api/persons', (request, response) => {
     const body = request.body
 
     if (!body) return response.status(400).json({error: 'missing content'})
+    if (!(body.name && body.number)) return response.status(400).json({error: 'missing information'})
+    if (persons.find(p => p.name === body.name)) return response.status(400).json({error: 'name must be unique'})
 
     const person = {
         name: body.name,
