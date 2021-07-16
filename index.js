2dfd4eab1d16f713e7da055377c3774135a268c8
Vesa <vesainwonderland@gmail.com>
2021-07-16 20:28:02 +0300

    3.16 puhelinluettelo ja tietokanta

--- index.js
+++ index.js
@@ -40,24 +40,28 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 //     },
 // ]
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(result => {
-        response.json(result)
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(result => {
+            response.json(result)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
-            response.json(person)
+            if (person) response.json(person)
+            response.status(404).end()
         })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has ${persons.length} people </p>` + new Date().toString())
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body) return response.status(400).json({ error: 'missing content' })
@@ -68,19 +72,22 @@ app.post('/api/persons', (request, response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const id = Number(request.params.id)
     const body = request.body
 
@@ -96,6 +103,22 @@ app.put('/api/persons/:id', (request, response) => {
     persons = persons.map(p => p.id !== id ? p : person)
     response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
