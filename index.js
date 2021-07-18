b01c126a91f4eee0ace40a6934c2a7dd78344f47
Vesa <vesainwonderland@gmail.com>
2021-07-18 22:09:21 +0300

    3.19 puhelinluettelo ja tietokanta

--- index.js
+++ index.js
@@ -17,29 +17,6 @@ morgan.token('request-content', function (request, response) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-content'))
 
-// let persons = [
-//     {
-//         "name": "Arto Hellas",
-//         "number": "040-123456",
-//         "id": 1
-//     },
-//     {
-//         "name": "Ada Lovelace",
-//         "number": "39-44-5323523",
-//         "id": 2
-//     },
-//     {
-//         "name": "Dan Abramov",
-//         "number": "12-43-234345",
-//         "id": 3
-//     },
-//     {
-//         "name": "Mary Poppendieck",
-//         "number": "39-23-6423122",
-//         "id": 4
-//     },
-// ]
-
 app.get('/api/persons', (request, response, next) => {
     Person.find({})
         .then(result => {
@@ -114,17 +91,17 @@ app.put('/api/persons/:id', (request, response, next) => {
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: 'name must be unique'})
     }
     next(error)
 }
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT
